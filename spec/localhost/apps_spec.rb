3f66d968b72e8da652a2871478846247b008ce49
Duane M. Moody <dmmoody@gmail.com>
2017-01-09 08:59:58 -0800

    add specs for homebrew packages needed to be installed

--- spec/localhost/apps_spec.rb
+++ spec/localhost/apps_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
+packages = %w(
+  macvim
+  git
+  tmux
+  pyenv
+  battery
+  heroku
+  hub
+  node
+  reattach-to-user-namespace
+  tree
+  the_silver_searcher
+  packer
+  jq
+  postgres
+  postgresql
+  redis
+)
+
 describe "required apps" do
   context "required services" do
     describe service("postgresql") do
@@ -14,8 +33,17 @@ describe "required apps" do
       it { should be_running }
     end
 
-    describe service("caffeine") do
+    describe service("Caffeine") do
       it { should be_running }
     end
   end
+
+  context "require packages" do
+    packages.each do |package|
+      describe package(package) do
+        it { should be_installed }
+      end
+    end
+  end
 end
+
